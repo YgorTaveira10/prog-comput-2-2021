732e7d7599064c789ea8d6b4cf9195f9d66544dc
Ygor Taveira <ygortaveira@outlook.com>
2021-09-14 22:22:27 -0200

    14-09

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,18 +1,60 @@
-function principal (){
+function principal(){
 
     f1()
     f2()
     f3()
-}
 
-function f1(){
-    alert(`Chamada de f1`)
-}
+    let opcao
+    let idade// escopo local -> FUNCIONA NO LOCAL ONDE FOI DEFINIDO
+    let peso, altura
+    let vetor = [] // declaração do vetor
+    do {
+        opcao = Number(prompt(`Digite 1. F1 2. F2 3. F3 4. F4 5. Sair`))
+        switch(opcao){
+            case 1: idade = Number(prompt(`Informe sua idade`))
+                    f1(idade)
+                    break
+            case 2: peso = Number(prompt(`Informe seu peso`))
+                    f2(idade, peso)
+                    break
+            case 3:  idade = Number(prompt(`Informe sua idade`))
+                    peso = Number(prompt(`Informe seu peso`))
+                    altura = Number(prompt(`Informe sua altura`))
+                    f3(idade, peso, altura)
+                    break
+            case 4: vetor.push(1) // adc 1
+                    vetor.push(2) // adc 2
+                    vetor.push(3) // adc 3
+                    f4(vetor) //passando um vetor como um parametro
+                    alert(vetor)
+                    break       
+            case 5: alert(`Programa será encerrado`)
+                    break
+            default: alert(`Opção inválida`)
+        }
+    }
+    while (opcao != 4 )
 
-function f2(){
-    alert(`Chamada de f2`)
 }
+function f1(idade){
 
-function f3(){
-    alert(`Chamada de f3`)
+
+    // let idade = 10
+     alert(`Valor de idade ${idade}`)
+     idade = idade + 10
+     return idade // retorna o resultado da função
+}
+// função que recebe parâmetro
+function f2(idade, peso){
+     alert(`Valor de idade ${idade} e do peso ${peso}`)
+}
+// função que recebe parâmetro
+function f3(idade, peso, altura){
+    alert(`Valor de idade ${idade}, valor do peso ${peso} e valor da altura ${altura}`)
+} 
+function f4(vetor){
+    //alterando o valor do vetor
+    vetor[0] = vetor[0] + 10
+    vetor[1] = vetor[1] + 10
+    vetor[2] = vetor[2] + 10
 }
--- js/sistemadevendas.js
+++ js/sistemadevendas.js
@@ -3,100 +3,134 @@ let sistemaVendas = () => {
     let opcao
     let vetVendedores = []
     let vetVendas = []
+    let codigo
+    let mes
+    
     do {
-        opcao = Number(prompt(`Informe: \n1. Cadastrar vendedor \n2. Cadastrar venda \n3 Procura venda \n4. Soma Venda/vendedor \n5. Sair`))
+        opcao = Number(prompt(`Informe: \n1. Cadastrar vendedor \n2. Cadastrar venda \n3. Procura venda por mês e vendedor \n4. Soma venda por vendedor \n5. Mostra a maior venda do mês \n6. Valor por mês \n7. Sair`))
         switch(opcao){
-            case 1: let objeto = {
-                        codigo: Number(prompt(`Informe código`)),
-                        nome: prompt(`Informe nome`),
-                        rg: prompt(`Informe rg`) // 23.646.356-1
-                    }
-                    // verifica se já tem outro vendedor com o mesmo código
-                    let achou = false
-                    // vetVendedores.length retorna o tamanho do vetor
-                    for(let i=0; i < vetVendedores.length;i++){
-                        if (vetVendedores[i].codigo == objeto.codigo){
-                            achou = true // achei - não posso cadastrar      
-                        }
-                    }
-                    if (!achou){
-                         // adiciona objeto no vetor
-                        vetVendedores.push(objeto)
-                        alert(`Vendedor cadastrado com sucesso`)
-                    }
-                    else {
-                        alert(`Vendedor já existe com este código`)	
-                    }
-                    console.log(vetVendedores)
+            case 1: cadastroVendedor(vetVendedores)
                     break
-            case 2: let objeto1 = {
-                        codigo: Number(prompt(`Informe código do vendedor`)),
-                        mes: Number(prompt(`Informe mês da venda`)),
-                        valor: Number(prompt(`Informe o valor da venda`)) 
-                    }
-                    let achou1 = false
-                    for(let i=0;i<vetVendas.length;i++){
-                        // nao podemos cadastrar duas vendas para o mesmo vendedor no mesmo mes
-                        if ((vetVendas[i].codigo == objeto1.codigo) && (vetVendas[i].mes == objeto1.mes)){
-                            achou1 = true // encontrei - não podemos cadastrar venda
-                        }
-                    }
-                    if (!achou1){
-                        vetVendas.push(objeto1)
-                    }
-                    else {
-                        alert(`Já existe vende deste vendedor neste mês`)
-                    }
-                    console.log(vetVendas)
+            case 2: cadastraVenda (vetVendas)
                     break
-            case 3: let codigo = Number(prompt(`Informe o codigo do vendedor`))
-                    let mes = Number(prompt(`Informe o mês da venda`))
-                    // percorre o vetor do vendas
-                    let achou3 = false
-                    for (let i=0; i<vetVendas.length; i++){
-                        if ((vetVendas[i].codigo == codigo) && (vetVendas[i].mes == mes)){
-                            console.log(`O valor da venda do funcionario ${codigo} no mes ${mes} foi ${vetVendas[i].valor}`)
-                            achou = true
-                        }                        
-                    }
-                    if (!achou3){
-                        console.log(`Venda não encontrada para este funcionario neste mes`)
-                    }
+            case 3: 
+                    codigo = Number(prompt(`Informe o código do vendedor`))
+                    mes = Number(prompt(`Informe o mês da venda`))
+                    consultaVendasCodigoMes(vetVendas, codigo, mes)
                     break
-            case 4: let codigo4 = Number(prompt(`Informe o codigo do vendedor`))
-                    // percorre o vetor de vendas
-                    let soma = 0
-                    let achou4 = false
-                    for(let i=0; i<vetVendas.length; i++){
-                        if(vetVendas[i].codigo == codigo4){
-                            soma = soma + vetVendas[i].valor
-                            achou4 = true
-                        }
-                    }
-                    if (soma == 0){
-                        console.log(`Venda não encontrada para este funcionario, ou venda igual a 0`)
-                    }
-                    else {
-                        console.log(`O total de vendas do vendedor ${codigo} foi ${soma}`)
-                    }
+            case 4: codigo = Number(prompt(`Informe o código do vendedor`))
+                    consultaVendasCodigo(vetVendas, codigo)
                     break
-            case 5: let mes5 = Number(prompt(`Informe o mes de interesse`))
-                    let maiorvalor = 0
-                    let vendedorMaisVendeu = 0
-                    for(let i=0; i<vetVendas.length; i++){
-                        if ( vetVendas[i].mes == mes5){
-                            if(vetVendas[i].valor > maiorvalor){
-                                maiorvalor = vetVendas[i].valor // atualiza seu valor
-                                vendedorMaisVendeu = vetVendas[i].codigo
-                            }
-                        }
-                    }
-                    console.log(`O vendedor que mais vendeu foi ${vendedorMaisVendeu} com ${maiorvalor}`)
-                    break                
-            case 6: alert(`O programa será encerrado`) 
+            case 5: mes = Number(prompt(`Informe o mês de interesse`))
+                    consultaVendedor(vetVendas, mes)
+                    break
+            case 6: consultaVendasMes(vetVendas)
+                    break
+            case 7: alert(`O programa será encerrado`)
                     break
             default: alert(`Opção inválida`)
         }
     }
-    while (opcao != 6)
+    while (opcao != 7)
+}
+
+let cadastroVendedor = (vetVendedores) => {
+    let objeto = {
+        codigo: Number(prompt(`Informe código`)),
+        nome: prompt(`Informe nome`),
+        rg: prompt(`Informe rg`) // 23.646.356-1
+    }
+    // verifica se já tem outro vendedor com o mesmo código
+    let achou = false
+    // vetVendedores.length retorna o tamanho do vetor
+    for(let i=0; i < vetVendedores.length;i++){
+        if (vetVendedores[i].codigo == objeto.codigo){
+            achou = true // achei - não posso cadastrar      
+        }
+    }
+    if (!achou){
+         // adiciona objeto no vetor
+        vetVendedores.push(objeto)
+        alert(`Vendedor cadastrado com sucesso`)
+    }
+    else {
+        alert(`Vendedor já existe com este código`)	
+    }
+    console.log(vetVendedores)
+}
+
+let cadastraVenda = (vetVendas) => {
+    let objeto = {
+        codigo: Number(prompt(`Informe código do vendedor`)),
+        mes: Number(prompt(`Informe mês da venda`)),
+        valor: Number(prompt(`Informe o valor da venda`)) 
+    }
+    let achou = false
+    for(let i=0;i<vetVendas.length;i++){
+        // não podemos cadastrar duas vendas para um mesmo vendedor no mesmo mês
+        if ((vetVendas[i].codigo == objeto.codigo) && (vetVendas[i].mes == objeto.mes)){
+            achou = true // encontrei - não podemos cadastrar venda
+        }
+    }
+    if (!achou){
+        vetVendas.push(objeto)
+    }
+    else {
+        alert(`Já existe vende deste vendedor neste mês`)
+    }
+    console.log(vetVendas)
+}
+
+let consultaVendasCodigoMes = (vetVendas, codigo, mes) => {
+    // percorre vetor de vendas
+    let achou = false
+    for(let i=0;i<vetVendas.length;i++){
+        if ((vetVendas[i].codigo == codigo) && (vetVendas[i].mes == mes)){
+            console.log(`O valor da venda do funcionário ${codigo} no mês ${mes} foi ${vetVendas[i].valor}`)
+            achou = true
+        }
+    }
+    if (!achou){
+        console.log(`Venda não encontrada para este funcionário neste mês`)
+    }
+}
+
+let consultaVendasCodigo = (vetVendas, codigo) => {
+    // percorre o vetor de vendas
+    let soma = 0
+    for(let i=0;i<vetVendas.length;i++){
+        if (vetVendas[i].codigo == codigo4){
+            soma = soma + vetVendas[i].valor
+        }
+    }
+    if (soma == 0){
+        console.log(`Venda não encontrada para este funcionário, ou venda igual o 0`)
+    }
+    else {
+        console.log(` O total de vendas do vendedor ${codigo4} foi ${soma}`)
+    }
+}
+
+let consultaVendedor = (vetVendas, mes) => {
+    let maiorValor = 0
+    let vendedorMaisVendeu = 0 
+    for(let i=0;i<vetVendas.length;i++){
+        if (vetVendas[i].mes == mes){
+            if (vetVendas[i].valor > maiorValor){
+                maiorValor = vetVendas[i].valor // atualiza o maior valor
+                vendedorMaisVendeu = vetVendas[i].codigo // atualiza vendedor
+            }
+        }
+    }+
+    console.log(`O vendedor que mais vendeu foi ${vendedorMaisVendeu} com ${maiorValor}`)
+}
+
+let consultaVendasMes = (vetVendas) => {
+    let vetMeses = [0,0,0,0,0,0,0,0,0,0,0,0]
+    // percorre o vetor de vendas
+    for(let i=0;i<vetVendas.length;i++){
+        let aux = vetVendas[i].mes // mês da venda
+        vetMeses[aux-1] = vetMeses[aux-1] + vetVendas[i].valor
+    }
+    console.log(vetMeses)
 }
